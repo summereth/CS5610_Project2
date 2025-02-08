ee5831ca7109a9144d6620d7914bcd6f47c19fd1
summereth <liq.qian.l@gmail.com>
2025-02-08 13:32:17 -0800

    [server] add endpoint /exercise/:id

--- db/exercises.js
+++ db/exercises.js
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 
 const collection_name = "exercises";
 
@@ -27,4 +27,31 @@ async function find(query = {}) {
   }
 }
 
-export { find };
+async function findById(id) {
+  const client = new MongoClient(process.env.MONGO_URI);
+
+  try {
+    // Connect to database
+    await client.connect();
+
+    // Get database and collection
+    const db = client.db(process.env.DB_NAME);
+    const collection = db.collection(collection_name);
+
+    // Find exercises
+    const exercises = await collection.findOne({
+      _id: ObjectId.createFromHexString(id),
+    });
+    return exercises;
+  } catch (error) {
+    console.error("Error getting exercise by ID:", error);
+    throw error;
+  } finally {
+    // Close the connection
+    if (client) {
+      await client.close();
+    }
+  }
+}
+
+export { find, findById };
--- routes/exerciseRoutes.js
+++ routes/exerciseRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { find } from "../db/exercises.js";
+import { find, findById } from "../db/exercises.js";
 
 const router = express.Router();
 router.get("/", getExercises);
-// router.get("/:id", getExerciseById);
+router.get("/:id", getExerciseById);
 
 async function getExercises(req, res) {
   const muscleGroup = req.query.muscleGroup;
@@ -17,18 +17,18 @@ async function getExercises(req, res) {
   }
 }
 
-// async function getExerciseById(req, res) {
-//   console.log("Getting exercise by ID");
-//   try {
-//     const exercise = await findById(req.params.id);
-//     if (exercise) {
-//       console.log(exercise);
-//       res.json(exercise);
-//     }
-//   } catch (error) {
-//     console.error("Error getting exercise by ID:", error);
-//     res.status(500).send("Error getting exercise by ID");
-//   }
-// }
+async function getExerciseById(req, res) {
+  console.log("Getting exercise by ID");
+  try {
+    const exercise = await findById(req.params.id);
+    if (exercise) {
+      console.log(exercise);
+      res.json(exercise);
+    }
+  } catch (error) {
+    console.error("Error getting exercise by ID:", error);
+    res.status(500).send("Error getting exercise by ID");
+  }
+}
 
 export default router;
